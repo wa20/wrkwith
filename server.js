65a9d7fcffdbce7f7c73ebfea9a47008f9b7a0c2
wael <78425538+wa20@users.noreply.github.com>
2021-06-02 17:15:52 +0100

    basic server created

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs =exphbs.create({});
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.engine('handle', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on ${PORT}.`));
+  });
