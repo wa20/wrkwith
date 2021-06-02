5ad6b447e417450cce08f11a6d9f6e7342e416b3
wael <78425538+wa20@users.noreply.github.com>
2021-06-02 20:20:02 +0100

    server fixes, now working

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,16 @@
 const router = require('express').Router();
+// const apiRoutes = require('./api');
+
+// router.use('/api', apiRoutes);
+
+// router.get('/', (req, res) => {
+//     if(req.session.loggedIn) {
+//         res.redirect('/');
+//         return;
+//     }
+//     res.render("login")
+// })
+
 
 router.get('/', async (req, res) => {
   
