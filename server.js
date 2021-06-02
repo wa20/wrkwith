efeb96ad24bf73c543b26e5131684444182c8b9a
Harith Albadry <harith.albadry@gmail.com>
2021-06-02 20:40:02 +0100

    server working

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const session = require("express-session")
 const hbs = exphbs.create({});
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+const sess = {
+secret: "super secret",
+cookie: {},
+resave: false,
+saveUninitialized: true,
+store: new SequelizeStore({
+    db: sequelize
+  })
+}
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.json())
+app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers/index'))
+app.use(routes);
 
-// sequelize.sync({ force: false }).then(() => {
+ sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on ${PORT}.`));
-//   });
+   });
