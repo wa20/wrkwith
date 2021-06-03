81abb812ea0f763d276adbe537429080fc0fcc75
Bipin <bipp2009@gmail.com>
2021-06-03 21:06:36 +0100

    BP

--- controllers/api/user.js
+++ controllers/api/user.js
@@ -1,30 +1,29 @@
-const router = require('express').Router();
-const { User } = require('../../models');
+const router = require("express").Router();
+const { User } = require("../../models");
 
+router.post("/signup", async (req, res) => {
+  try {
+    const newuser = await User.create({
+      user_name: req.body.userName,
+      email: req.body.email,
+      password: req.body.password,
+      first_name: req.body.firstName,
+      last_name: req.body.lastName,
+    });
+    res.json(newuser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-router.post("/signup" , async (req,res)=>{
-    try{
-        const newuser=await User.create({
-            user_name: req.body.userName,
-            email: req.body.email,
-            password: req.body.password,
-            first_name:req.body.firstName,
-            last_name:req.body.lastName
-        })
-        res.json(newuser)
-    } catch (err) {
-        res.status(500).json(err)
-    }
-})
+router.get("/community", async (req, res) => {
+  try {
+    const userdata = await User.findAll();
+    const users = userdata.map((user) => user.get({ plain: true }));
+    res.render("community", { users });
+  } catch (err) {
+    console.log("we hit this error here" + err);
+  }
+});
 
-router.get("/community", async (req,res) => {
-    try {
-        const userdata= await User.findAll();
-        const users = userdata.map((user)=>user.get({plain:true}));
-        res.render("community",{users});
-    } catch(err){
-        console.log("we hit this error here" + err)
-    }
-})
-
-module.exports = router
+module.exports = router;
