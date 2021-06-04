8c79686d58a4752e267a3dbcc53bab681a135eb8
wael <78425538+wa20@users.noreply.github.com>
2021-06-04 21:17:36 +0100

    login js feature created

--- public/js/login.js
+++ public/js/login.js
@@ -1,1 +1,10 @@
-const Login
+const Login = document.getElementById("loginUser")
+
+Login.addEventListener("click", async (event)=>{
+
+    event.preventDefault();
+
+
+    const Username = document.getElementById("loginUsername").value.trim();
+    const Password = document.getElementById("loginPassword").value.trim();
+})
