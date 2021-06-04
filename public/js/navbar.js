e80cebf689e03991b519f0178d71007460cbc45e
wael <78425538+wa20@users.noreply.github.com>
2021-06-04 09:37:20 +0100

    partials file added

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -8,7 +8,25 @@
 //     scrollVal = x
 // });
 
+// const { strictLeft } = require("sequelize/types/lib/operators")
 
-$(window).scroll(function(){
-   $('nav').toggleClass('scrolled', $(this).scrollTop() > 900)
-});
+
+// $(window).scroll(function(){
+//    $('nav').toggleClass('scrolled', $(this).scrollTop() > 900)
+// });
+
+"use strict";
+
+const htmlTag = document.querySelector('html');
+const bodyTag = document.querySelector('body');
+const myNav  = document.getElementById('navbarScroll')
+const bodyTag = document.querySelector('nav li a');
+
+let scrolled = () => {
+    let dec = scrollY / (bodyTag.scrollHeight - innerHeight);
+    return Math.floor(dec * 10);
+}
+
+addEventListener('scroll', () =>{
+    myNav.style.setProperty('backgroud', scrolled() > 50 ? "var(--color2)" : "var(--color1)")
+})
