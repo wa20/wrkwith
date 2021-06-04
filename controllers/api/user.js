3890b324811cfeb88389936bd30dfe933ae116ed
wael <78425538+wa20@users.noreply.github.com>
2021-06-04 22:22:27 +0100

    updated login js file and user controller for login function

--- controllers/api/user.js
+++ controllers/api/user.js
@@ -17,6 +17,45 @@ router.post("/signup" , async (req,res)=>{
     }
 })
 
+//login function
+router.post('/login', async (req, res) =>{
+    try{
+        const userData = await User.findOne({
+            where: {username: req.body.username}
+        });
+
+        if (!userData) {
+            res
+                .status(400)
+                .json({message: "Incorrect email or password, please try again"});
+                return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+    
+        if(!validPassword) {
+            res
+                .status(400)
+                .json({message:"Incorrect email or password, please try again"});
+                return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+        res.json({user: userData, message: "Lgged in"})
+        }); 
+        
+    } catch (err) {
+        res.status(400).json(err);
+    }
+
+});
+
+
+
+//community page
 router.get("/community", async (req,res) => {
     try {
         const userdata= await User.findAll();
--- public/js/login.js
+++ public/js/login.js
@@ -7,4 +7,23 @@ Login.addEventListener("click", async (event)=>{
 
     const Username = document.getElementById("loginUsername").value.trim();
     const Password = document.getElementById("loginPassword").value.trim();
-})
+
+    if (Username && Password) {
+
+        const response = await fetch ('/api/user/login', {
+            method: 'POST',
+            body: JSON.stringify({
+                userName: Username,
+                password: Password,
+            }),
+            headers: {'Content-Type': 'application/json'},
+        });
+
+        if(response.ok){
+            document.location.replace('/profile');
+        } else {
+            alert(response.statusText)
+        }
+    }
+
+});
