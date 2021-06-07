4c64da336707ac39713ef999d74beab20f3d24ca
wael <78425538+wa20@users.noreply.github.com>
2021-06-07 17:14:09 +0100

    created a logout js file to render logout function and added logout route to controller index

--- controllers/index.js
+++ controllers/index.js
@@ -50,4 +50,26 @@ router.get('/profile', withAuth, async (req, res) => {
     }
   });
 
+
+  //edit profile page
+  router.get('/editaccount', async (req, res) => {
+    res.render('profileEditDetails')
+  })
+
+  //edit profile image page
+  router.get('/editimage', async (req, res) => {
+    res.render('profileEditImage')
+  })
+
+//logout
+  router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
+
 module.exports = router
