c719029db01d4e0d754ca8ed148cb3514b71ef78
Harith Albadry <harith.albadry@gmail.com>
2021-06-09 23:23:43 +0100

    all project page now has functioning cards with user info and pics

--- controllers/html/allprojects.js
+++ controllers/html/allprojects.js
@@ -5,8 +5,17 @@ const { Project,User, Collab } = require("../../models");
 // find all projects 
 router.get("/", async (req, res) => {
     try {
-      const projectdata = await Project.findAll();
-      const projects = projectdata.map((project) => project.get());
+      const projectdata = await Project.findAll({
+        include:[
+          {
+            model:User,
+            attributes:["user_name","avatar"]
+          }
+        ]
+      });
+      const projects = projectdata.map((project) => project.get({plain:true}));
+      const project2 = JSON.stringify(projects)
+      console.log("/////////////////// projects are " + project2)
       res.render("projects", { projects });
     } catch (err) {
       console.log("we hit this error here" + err);
