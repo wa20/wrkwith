935a046d815d324411830e6feb1230c48b5a5f1a
wael <78425538+wa20@users.noreply.github.com>
2021-06-09 14:45:50 +0100

    added routes for profile edit pages

--- controllers/html/index.js
+++ controllers/html/index.js
@@ -3,15 +3,22 @@ const router = require("express").Router();
 const communityRoute = require("./community");
 const logoutRoute = require("./logout");
 const profileRoute = require("./profile");
+const editProfileRoute = require("./profile-edit")
+const editProfileImageRoute = require("./profile-image")
 const oneProject = require("./projectfindone");
 const allProjects = require("./allprojects");
 const homeRoute = require("./homepage")
 const signupRoute = require("./signup")
 const loginRoute = require("./login")
 
+
+
+//routes
 router.use("/community", communityRoute);
 router.use("/logout", logoutRoute);
 router.use("/profile", profileRoute);
+router.use("/profile-edit", editProfileRoute);
+router.use("/profile-image", editProfileImageRoute);
 router.use("/project",oneProject);
 router.use("/projects",allProjects);
 router.use("/homepage",homeRoute);
