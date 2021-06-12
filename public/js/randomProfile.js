2038f0202a0681b83e564c70ebd66d12ffb03edb
wael <78425538+wa20@users.noreply.github.com>
2021-06-12 01:05:26 +0100

    j query issue fixed

--- public/js/randomProfile.js
+++ public/js/randomProfile.js
@@ -1,30 +1,29 @@
-window.onload = profile;
-
 var avatars = [
 
-    "elliot",
-    "jenny",
-    "joe",
-    "aura",
-    "steve",
-    "adi",
-    "chris",
-    "christian",
-    "becky",
-    "matthew",
-    "molly",
-    "elyse",
+  "elliot",
+  "jenny",
+  "joe",
+  "aura",
+  "steve",
+  "adi",
+  "chris",
+  "christian",
+  "becky",
+  "matthew",
+  "molly",
+  "elyse",
 
 ];
+  
+
+
+window.onload = function() {
     
+    let random = Math.floor(Math.random() * avatars.length);
+  
+    document.getElementById("avatar").src = `/assets/profiles/${avatars[random]}.jpg`;
+  };
 
+  
 
-    function profile(){
-      
-      let random = Math.floor(Math.random() * avatars.length);
-    
-      document.getElementById("avatar").src = `/assets/profiles/${avatars[random]}.jpg`
-    };
-
-    // profile()
-    console.log(profile)
+ 
