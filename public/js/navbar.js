c122d47b0f145ae7cf77d2ad0c2764d68b98bb80
wael <78425538+wa20@users.noreply.github.com>
2021-06-13 01:03:01 +0100

    slight navbar changes made

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,60 +1,18 @@
 // "use strict";
 
-// const htmlTag = document.querySelector('html');
-// const bodyTag = document.querySelector('body');
-// const myNav  = document.getElementById('navbarScroll')
-// const bodyTag = document.querySelector('nav li a');
+const  myNav = document.getElementById('mynav');
+window.addEventListener('scroll', (event) =>  { 
+    "use strict";
+    if (window.scrollY > 10 ) {
+        myNav.classList.add("nav-dark");
+    } 
+    else {   
+        myNav.classList.remove("nav-dark");
+    }
 
-// let scrolled = () => {
-//     let dec = scrollY / (bodyTag.scrollHeight - innerHeight);
-//     return Math.floor(dec * 10);
-// }
+});
 
-// addEventListener('scroll', () =>{
-//     myNav.style.setProperty('backgroud', scrolled() > 50 ? "var(--color2)" : "var(--color1)")
-// })
-
-
-// const header = document.getElementById('navbar-colour');
-// const sectionOne= document.getElementById('page-intro');
-
-// const sectionOneOptions = {
-//     rootMargin: "-200px 0px 0px 0px"
-// }
-// navbarcolour
-// const sectionOneOption2 = {
-//     rootMargin: "-0px 0px 0px 0px"
-// }
-
-// const sectionOneObserver = new IntersectionObserver(function(entries, sectionOneObserver){
-//     enteries.forEach(entry => {
-//         if(!entry.isIntersecting){
-//             header.classList.add("nav-scrolled")
-//         }else {
-//             header.classList.remove("nav-scrolled")
-//         }
-//     })
-// })
-
-// sectionOneObserver.observe(sectionOne)
-
-
-window.onscroll = () => {
-    const nav = document.getElementById('navbar');
-    if(this.scrollY <= 50) nav.className = ''; else nav.className = 'scroll';
-  };
-
-// $(function() {
-//     var header = $("nav");
-  
-//     $(window).scroll(function() {    
-//         var scroll = $(window).scrollTop();
-//         if (scroll <= -50) {
-//             header.addClass("scrolled");
-//         } else {
-//             header.removeClass("scrolled");
-//         }
-//     });
-  
-// });
+window.addEventListener('scroll', ()=> {
+  console.log('scrolled')
+})
 
